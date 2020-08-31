a79f2f22cc0c8cb97329aa6a31601b6525152375
Fabio Andrade <f.andrade@reply.com>
2020-08-30 21:05:11 -0300

    asd

--- Body.js
+++ Body.js
@@ -1,11 +1,30 @@
-import React, {useState} from 'react';
-import { Text, Picker, View, font, ScrollView, StyleSheet, Image } from 'react-native';
+import React, {
+    useState
+} from 'react';
+import {
+    Text,
+    Picker,
+    View,
+    font,
+    ScrollView,
+    StyleSheet,
+    Image
+} from 'react-native';
 
-const Body = () =>{
-    return(
-        <Text style={{textAlign:'center', paddingBottom:10, fontWeight:'bold', backgroundColor:'#f5f5f5', width:'100%'}}>
-            Selecione o seu cardápio:
-        </Text>    
+const Body = () => {
+    return ( <
+        Text style = {
+            {
+                textAlign: 'center',
+                paddingBottom: 10,
+                fontWeight: 'bold',
+                backgroundColor: '#f5f5f5',
+                width: '100%'
+            }
+        } >
+        Selecione o seu cardápio 2:
+        <
+        /Text>    
     );
 }
 
